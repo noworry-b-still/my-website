a2125ec91da029dd387291a8cbf08c6de083044f
lord-cosmos <teslaatozinspires7@gmail.com>
2024-03-05 12:05:45 -0500

    put banner fixed - to show progress

---
+++ src/ui/comp/banner/Banner.tsx
@@ -0,0 +1,10 @@
+import "./Banner.css";
+function Banner() {
+  return (
+    <div className="banner">
+      Made with React by Dinesh Pandikona - Development in Progress
+    </div>
+  );
+}
+
+export default Banner;
