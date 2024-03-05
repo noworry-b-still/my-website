6f7017c19ae87e0b774bdbbfb6020bde2c9ac58f
lord-cosmos <teslaatozinspires7@gmail.com>
2024-03-05 16:50:03 -0500

    initialize projects page

--- src/page/projects.tsx
+++ src/page/projects.tsx
@@ -1,5 +1,22 @@
+import ProjectCard from "../ui/comp/project-card/ProjectCard"; // Import the ProjectCard component
+import projectsData from "../data/projects"; // Import the dummy projects data
+import "./projects.css";
+
 function Projects() {
-  return <></>;
+  return (
+    <div className="projects">
+      {projectsData.map((project, index) => (
+        <ProjectCard
+          key={index}
+          name={project.name}
+          description={project.description}
+          technologies={project.technologies}
+          image={project.image}
+          link={project.link}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Projects;
