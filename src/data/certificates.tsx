a5b8c3867dd9a34516ea0cece9acb20eaf8b8f45
lord-cosmos <teslaatozinspires7@gmail.com>
2024-03-05 20:50:37 -0500

    add certificates data

---
+++ src/data/certificates.tsx
@@ -0,0 +1,15 @@
+const certificates = [
+  {
+    name: "Certificate Name 1",
+    imageUrl: "url-to-image-1",
+    description: "Description for Certificate 1",
+  },
+  {
+    name: "Certificate Name 2",
+    imageUrl: "url-to-image-2",
+    description: "Description for Certificate 2",
+  },
+  // Add more certificates as needed
+];
+
+export default certificates;
--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,38 +1,46 @@
+import solar_dashboard from "../images/solar-dashboard.png";
+import image_editor from "../images/image-editor.png";
+
 const projects = [
   {
-    name: "Solar Site Monitoring Application",
+    name: "Solar Site Dashboard",
     description:
-      "Created solar monitoring app with Express, React, Redis, and Rest API, enabling geospatial site search.",
+      'Worked on implementing features like geospatial search, leader board for top solar locations as part of "Redis Certification" Journey.',
     technologies: [
       "Express",
-      "React",
+      "Node",
       "Redis",
-      "Rest API",
+      "Git",
       "Data Visualization",
       "DAO design pattern",
     ],
-    image:
-      "https://dummyjson.com/image/400x200/008080/ffffff?text=Hello+Peter!&fontFamily=cookie", // Image URL
+    image: solar_dashboard, // Image URL
     link: "https://dineshpandikona-references.netlify.app/", // Project URL
   },
   {
     name: "Image Editor - Java desktop GUI",
     description:
-      "Constructed a versatile image editing app in Java using Swing, supporting various formats (ppm, jpg, png).",
-    technologies: ["Java", "Swing", "Command design pattern", "Junit"],
-    image:
-      "https://dummyjson.com/image/400x200/008080/ffffff?text=Hello+Peter!&fontFamily=cookie", // Image URL
+      'Constructed a versatile image editing app in Java using Swing, supporting various formats (ppm, jpg, png) as part of "Programming Design Patterns" Journey.',
+    technologies: [
+      "Java",
+      "Swing",
+      "JUnit4",
+      "OOP principles",
+      "Git",
+      "Command design pattern",
+    ],
+    image: image_editor,
     link: "https://dineshpandikona-references.netlify.app/", // Project URL
   },
-  {
-    name: "Image Picker - Typescript Plugin",
-    description:
-      "Designed and created an innovative, reusable image picker plugin. It provides a highly responsive, customizable solution for efficiently selecting, displaying, and arranging images in a responsive layout.",
-    technologies: ["Typescript", "React", "Rollup", "lodash"],
-    image:
-      "https://dummyjson.com/image/400x200/008080/ffffff?text=Hello+Peter!&fontFamily=cookie", // Image URL
-    link: "https://dineshpandikona-references.netlify.app/", // Project URL
-  },
+  // {
+  //   name: "Image Picker - Typescript Plugin",
+  //   description:
+  //     "Designed and created an innovative, reusable image picker plugin. It provides a highly responsive, customizable solution for efficiently selecting, displaying, and arranging images in a responsive layout.",
+  //   technologies: ["Typescript", "React", "CSS3", "Git", "Rollup", "lodash"],
+  //   image:
+  //     "https://dummyjson.com/image/400x200/008080/ffffff?text=Hello+Peter!&fontFamily=cookie", // Image URL
+  //   link: "https://dineshpandikona-references.netlify.app/", // Project URL
+  // },
 ];
 
 export default projects;
