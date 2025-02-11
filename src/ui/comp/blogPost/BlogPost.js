aa08f7c1943a605e6dc702f15932b59d860fd1db
noworry-b-still <teslaatozinspires7@gmail.com>
2025-02-10 19:27:24 -0500

    responsive and color

--- src/ui/comp/blogPost/BlogPost.js
+++ src/ui/comp/blogPost/BlogPost.js
@@ -1,17 +1,16 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"; // Import useParams to access the slug
-import "./BlogPost.css";
+import { useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
-import "highlight.js/styles/github.css"; // You can use any highlight.js theme.
+import "highlight.js/styles/github.css"; // Code highlighting
+import "./BlogPost.css"; // Updated CSS file
 
 const BlogPost = () => {
-  const { slug } = useParams(); // Get the slug from the route parameters
+  const { slug } = useParams();
   const [content, setContent] = useState("");
 
   useEffect(() => {
-    // Fetch the markdown file for the blog post
     import(`../../posts/${slug}.md`)
       .then((res) => {
         fetch(res.default)
@@ -22,12 +21,14 @@ const BlogPost = () => {
   }, [slug]);
 
   return (
-    <div className="blog-post">
-      <ReactMarkdown
-        children={content}
-        remarkPlugins={[remarkGfm]}
-        rehypePlugins={[rehypeHighlight]}
-      />
+    <div className="blog-post-container">
+      <article className="blog-post">
+        <ReactMarkdown
+          children={content}
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeHighlight]}
+        />
+      </article>
     </div>
   );
 };
