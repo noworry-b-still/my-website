de0b4dc19074a737866361fbd5960296a547a2f3
noworry-b-still <teslaatozinspires7@gmail.com>
2025-04-13 23:54:48 -0400

    fix mazy_ai details/link

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,5 +1,6 @@
 import image_editor from "../images/pdp-image-app-thumbnail.png";
 import last_stand from "../images/the_last_stand.png"
+import mazy_ai_logo from "../images/mazy_ai_logo.png"
 
 
 const projects = [
@@ -9,41 +10,30 @@ const projects = [
       'Constructed a versatile image editing app in Java using Swing, supporting various formats (ppm, jpg, png) as part of "Programming Design Patterns" Journey.',
     technologies: [
       "Java",
-      "Swing",
-      "JUnit4",
-      "OOP principles",
-      "Git",
-      "Command design pattern",
+      "Software Design Patterns"
     ],
     image: image_editor,
-    link: "https://drive.google.com/file/d/1kqwfI5aHZ4-aXdKJdhU-ywNBeGPjNCrt/view?usp=drive_link", // Project URL
+    link: "https://drive.google.com/file/d/1WZhspbIsPzBYc7-QGe9IkcDla4HbqzeP/view?usp=sharing", // Project URL
   },
   {
-    name: "Maze Solver with Visualizations",
+    name: "MAZY AI - Complex Maze Solver",
     description:
-      'Engineered an interactive maze-solving tool using Ant Colony Optimization, featuring real-time pathfinding visualizations to showcase algorithmic efficiency.',
+      'Engineered an interactive maze-solving tool using various algorithms, featuring real-time pathfinding visualizations to showcase algorithmic efficiency.',
     technologies: [
       "Python",
-      "Pygame",
-      "Ant Colony Optimization",
-      "Git",
-      "Algorithm Design",
-      "Visualization"
+      "MacOS Bundle"
     ],
-    image: "https://picsum.photos/200",
-    link: "https://drive.google.com/file/d/your_maze_solver_link_here/view?usp=drive_link", // Replace with your actual project URL
+    image: mazy_ai_logo,
+    link: "https://drive.google.com/file/d/1pqTshLKNvKs4SaL0Wvo4zA10Ltt88b1M/view?usp=sharing",
   },
   {
     name: "The Last Stand",
     description:
       'The Last Stand: Earth\'s final hope rests in your hands—blast through alien fleets in an adrenaline-fueled space battle for survival!',
     technologies: [
-      "Unity",
       "C#",
-      "Scripting",
       "Game Development",
-      "Git",
-      "Game Design"
+
     ],
     image: last_stand,
     link: "https://drive.google.com/file/d/1xIZKMM6NC_Engq085BFmmIYhFQAybOCQ/view", // Project URL
